f102622559b5cd8770c83fe74f64498dac3ffb0a
Samantha Johnston <samareimorgan@gmail.com>
2018-10-21 14:23:02 -0600

    update cast array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,12 @@
 
-//Variable to hold the friend array data
-var friends = [
+//Variable to hold the cast array data
+
+var cast = [
     {
         name: "Rocky",
         favorite_color: "gold",
-        picture_Url: 
+        quote: "...[Grunts]...",
+        image: "../public/assets/images/rocky.jpg",
         answers: [
             1,
             2,
@@ -15,13 +17,14 @@ var friends = [
             1,
             1,
             2
-
         ]
     },
+
     {
         name: "Magenta",
         favorite_color: "magenta",
-        picture_Url: 
+        quote: "I'm lucky, he's lucky, we're all lucky!",
+        image: "../public/assets/images/magenta.jpg",
         answers: [
             2,
             4,
@@ -38,7 +41,8 @@ var friends = [
     {
         name: "Brad Majors",
         favorite_color: "gray",
-        picture_Url:
+        quote: "I'm glad we caught you at home. Could we use your phone? We're both in a bit of a hurry. We'll just say where were are and go back to the car. We don't want to be any worry.",
+        image: "../public/assets/images/Brad.jpg",
         answers: [
             2,
             1,
@@ -56,7 +60,8 @@ var friends = [
     {
         name: "Frank N Furter",
         favorite_color: "black",
-        picture_Url:
+        quote: "I'm just a sweet transvestite, from Transsexual Transylvania.",
+        image:"../public/assets/images/FrankNFurter.jpg",
         answers:[
             5,
             4,
@@ -74,7 +79,8 @@ var friends = [
     {
         name: "Janet Weiss",
         favorite_color: "white",
-        picture_Url:
+        quote: "If only we were amongst friends... or sane persons!",
+        image:"../public/assets/images/Janet.jpg",
         answers: [
             3,
             1,
@@ -93,7 +99,8 @@ var friends = [
     {
         name: "Columbia",
         favorite_color:"orange",
-        picture_Url:
+        quote: "Oh, slowly, slowly! It's too nice a job to rush.",
+        image:"../public/assets/images/Columbia.jpg",
         answers: [
             3,
             4,
@@ -107,12 +114,14 @@ var friends = [
             3
 
         ]
-    }
+    },
     {
         name: "Riff Raff",
         favorite_color: "black",
-        picture_Url: 
-        answers: [
+        quote: "You've arrived on a rather special night. It's one of the master's affairs.",
+        image: "../public/assets/images/riff raff.jpg",
+        answers: 
+        [
             1,
             3,
             2,
@@ -130,4 +139,4 @@ var friends = [
 
 
 ]
-module.exports = friendArray;
+module.exports = castArray;
